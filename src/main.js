81626a306f4b9b196244242480cccaedfbf458a8
railombardi <raidslombardi@gmail.com>
2022-06-15 22:50:05 -0300

    initialize project config

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
+
   render: function (h) {
     return h(App);
   },
